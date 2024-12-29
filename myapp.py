f52add663a3060f17cc95f5aadfcaa8d605a21c0
afroz patel <afroz.patel2003@gmail.com>
2024-12-29 20:31:40 +0530

    hello

---
+++ myapp.py
@@ -0,0 +1,12 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def root():
+    return "welcome to catalog service"
+
+
+app.run(host='0.0.0.0', port=4000, debug=True)
+
